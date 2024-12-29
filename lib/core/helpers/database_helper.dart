f7deff7c75c8cb782b5ff5d218fee710b1426de7
DStudentDeveloper <int22011532@central.edu.gh>
2024-12-29 23:58:43 +0000

    ✨ Feat: Write a `DatabaseHelper`

---
+++ lib/core/helpers/database_helper.dart
@@ -0,0 +1,57 @@
+import 'package:available/core/interfaces/model.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  late Database db;
+
+  static const createUserTableQuery = '''
+    CREATE TABLE users(
+      id TEXT NOT NULL,
+      studentEmail TEXT NOT NULL PRIMARY KEY,
+      name TEXT NOT NULL,
+      courseId TEXT NOT NULL,
+      courseName TEXT NOT NULL,
+      facultyId TEXT NOT NULL,
+      facultyName TEXT NOT NULL,
+      indexNumber TEXT NOT NULL,
+      levelId TEXT NOT NULL,
+      levelName TEXT NOT NULL
+    )
+  ''';
+
+  Future<void> open(String path) async {
+    db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database db, int version) async {
+        await db.execute(createUserTableQuery);
+      },
+    );
+  }
+
+  Future<int> insert<T extends Model>(T model) async {
+    final result = await db.insert(
+      'users',
+      model.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+    return result;
+  }
+
+  Future<T?> get<T extends Model>({
+    required String table,
+    required String key,
+    required String value,
+  }) async {
+    final result = await db.query(
+      table,
+      where: '$key = ?',
+      whereArgs: [value],
+      limit: 1,
+    );
+
+    if (result.isEmpty) return null;
+
+    return result.first as T;
+  }
+}
