a0dca106b836037a6c3caff3d3c9729976d8cace
DStudentDeveloper <int22011532@central.edu.gh>
2025-01-01 02:39:15 +0000

    ✨ Feat: Swap the socket.io client to use `socket_io_client` package

--- lib/core/services/socket_service.dart
+++ lib/core/services/socket_service.dart
@@ -1,74 +1,57 @@
-import 'dart:convert';
 import 'dart:developer';
 
 import 'package:available/core/common/app/state/availability_controller.dart';
 import 'package:available/core/utils/typedefs.dart';
 import 'package:available/src/booking/data/models/booking_model.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:socket_io_client/socket_io_client.dart';
 
 class SocketService {
   SocketService({
     required String url,
     required AvailabilityController availabilityController,
-  })  : _channel = WebSocketChannel.connect(Uri.parse(url)),
+  })  : _socket = io(url, OptionBuilder().setTransports(['websocket']).build()),
         _availabilityController = availabilityController {
     log('Attempting to connect to socket server', name: 'SocketService');
-    _channel.ready.then(
-      (_) {
-        log(
-          'Connected to socket server',
-          name: 'SocketService',
-          time: DateTime.now(),
-        );
-      },
-      onError: (Object error) {
-        log(
-          'Error connecting to socket server: $error',
-          name: 'SocketService',
-          time: DateTime.now(),
-        );
-      },
-    );
-    _channel.stream.listen((data) {
-      final jsonData = jsonDecode(data as String) as DataMap;
-      final event = jsonData['event'] as String?;
-      if (event == 'bookingUpdate') {
+
+    _socket
+      ..on('connect', (_) {
+        log('Connected to socket server', name: 'SocketService');
+        subscribeToClasses();
+      })
+      ..onDisconnect((_) {
+        log('Disconnected from socket server', name: 'SocketService');
+      })
+      ..on('subscribed', (_) {
+        log('Subscribed to classes', name: 'SocketService');
+      })
+      ..on('unsubscribed', (_) {
+        log('Unsubscribed from classes', name: 'SocketService');
+      })
+      ..on('bookingUpdate', (data) {
+        data as DataMap;
+        log(data.toString(), name: 'SocketService');
         _availabilityController.updateRoomAvailabilityCache(
-          roomId: jsonData['classId'] as String,
-          updatedBookings: List<DataMap>.from(jsonData['bookings'] as List)
+          roomId: data['classId'] as String,
+          updatedBookings: List<DataMap>.from(data['bookings'] as List)
               .map(BookingModel.fromMap)
               .toList(),
         );
-      } else if (event == 'subscribed') {
-        log(
-          'Subscribed to classes',
-          name: 'SocketService',
-          time: DateTime.now(),
-        );
-      } else if (event == 'unsubscribed') {
-        log(
-          'Unsubscribed from classes',
-          name: 'SocketService',
-          time: DateTime.now(),
-        );
-      }
-    });
+      });
   }
 
-  final WebSocketChannel _channel;
+  final Socket _socket;
   final AvailabilityController _availabilityController;
 
   void subscribeToClasses() {
-    _channel.sink.add(jsonEncode({'action': 'subscribe'}));
+    _socket.emit('subscribe');
   }
 
   void unsubscribeFromClasses() {
-    _channel.sink.add(jsonEncode({'action': 'unsubscribe'}));
+    _socket.emit('unsubscribe');
   }
 
   void dispose() {
     unsubscribeFromClasses();
-    _channel.stream.listen((_) {}).cancel();
-    _channel.sink.close();
+    _socket.dispose();
   }
 }
