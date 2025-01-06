920380cc3529d333aa9aa21605f24475511d1186
DStudentDeveloper <int22011532@central.edu.gh>
2025-01-06 00:09:51 +0000

    ✅ Build: Add navigator keys to `core_constants.dart`

--- lib/core/utils/core_constants.dart
+++ lib/core/utils/core_constants.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/widgets.dart';
+
 sealed class CoreConstants {
   const CoreConstants();
 
   static const userTable = 'users';
   static const userPrimaryKey = 'studentEmail';
+  static final rootNavigatorKey = GlobalKey<NavigatorState>();
+  static final nestedNavigatorKey = GlobalKey<NavigatorState>();
 }
