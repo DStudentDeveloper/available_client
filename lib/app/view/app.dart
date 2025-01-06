117d22a49da3f1074de373b6b966db5820d418df
DStudentDeveloper <int22011532@central.edu.gh>
2025-01-06 00:14:11 +0000

    ✅ Build: Configure `app.dart`

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,6 +1,10 @@
-import 'package:available/counter/counter.dart';
+import 'package:available/core/extensions/context_extensions.dart';
+import 'package:available/core/res/colours.dart';
+import 'package:available/core/services/router.dart';
+import 'package:available/core/utils/core_constants.dart';
 import 'package:available/l10n/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -8,15 +12,39 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Available',
+      navigatorKey: CoreConstants.rootNavigatorKey,
       theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colours.primary),
         useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        textTheme: GoogleFonts.interTextTheme(context.theme.textTheme),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colours.primary,
+            foregroundColor: Colors.white,
+            minimumSize: const Size(0, 50),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(
+              color: const Color(0xFF3E2121).withValues(alpha: .5),
+            ),
+          ),
+          contentPadding: const EdgeInsets.all(10),
+        ),
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      onGenerateRoute: routerConfig,
     );
   }
 }
