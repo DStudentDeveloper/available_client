733719b467b5bd747329e4741cd40ecceb3bfa23
DStudentDeveloper <int22011532@central.edu.gh>
2025-01-06 00:09:30 +0000

    ✅ Build: Initialise the cache in `bootstrap.dart`

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
 import 'dart:developer';
 
+import 'package:available/core/helpers/cache_helper.dart';
 import 'package:available/core/services/injection_container.dart';
 import 'package:available/firebase_options.dart';
 import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppBlocObserver extends BlocObserver {
   const AppBlocObserver();
@@ -34,6 +36,7 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await init();
+  CacheHelper.instance.init(sl<SharedPreferences>());
 
   runApp(await builder());
 }
